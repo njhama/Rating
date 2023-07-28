694a973a9a273568d29578a4836698e4c65a8089
Nicky Hama <njhama@usc.edu>
2023-07-28 12:04:54 -0700

    data

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 $(document).ready(function () {
     $('#table_id').DataTable({
+        order: [[0, 'desc']],
+        paging: false,
        "ajax": "data.json",
        "columns": [
+        {"data": "rating"},
         {"data": "name"},
         {"data": "artist"},
         {"data": "album"},
-        {"data": "rating"}
+       
        ]
         
     });
