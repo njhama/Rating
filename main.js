70e9a424af1f167c780d5835f1c0e2ce3a4d2c79
Nicky Hama <Nicky.Hama@millennium-space.com>
2023-07-28 12:29:43 -0700

    minor

--- main.js
+++ main.js
@@ -2,13 +2,14 @@ $(document).ready(function () {
     $('#table_id').DataTable({
         order: [[0, 'desc']],
         paging: false,
+        dom: 'Bfrtip',
+        buttons: ['excel', 'pdf', 'print'],
        "ajax": "data.json",
        "columns": [
-        {"data": "rating"},
-        {"data": "name"},
-        {"data": "artist"},
-        {"data": "album"},
-       
+            {"data": "rating"},
+            {"data": "name"},
+            {"data": "artist"},
+            {"data": "album"},
        ]
         
     });
